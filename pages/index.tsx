a959f9838040a62f74133a7546d3d8d238b032f0
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-30 10:20:50 +0300

    Sanity is schema is updated

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is a place to write, read, and connect
+          </h1>
+          <h2>
+            Its's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://www.nicepng.com/png/detail/48-483810_medium-logo-medium-social-media-icon-png.png"
+          alt=""
+        />
+      </div>
+      {/* posts */}
+    </div>
+  );
+};
+
+export default Home;
