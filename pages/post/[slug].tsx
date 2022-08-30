301c47489d975a2fe4bb870ce1e1af95fe08117a
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-30 13:38:23 +0300

    static site generation is started

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,33 @@
+import Header from "../../components/Header";
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
+function Post() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    slug{
+        current
+    }
+}`;
+
+  const posts = await sanityClient.fetch(query);
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking", //blocks page or shows 404 page if 404 page exists
+  };
+};
--- sanity.js
+++ sanity.js
@@ -1,9 +1,5 @@
-import {
-  createClient,
-  createCurrentUserHook,
-  createImageUrlBuilder,
-} from "next-sanity";
-
+import { createClient, createCurrentUserHook } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
@@ -12,5 +8,6 @@ export const config = {
 };
 
 export const sanityClient = createClient(config);
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+//export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export const urlFor = (source) => imageUrlBuilder(config).image(source);
 export const useCurrentUser = createCurrentUserHook(config);
